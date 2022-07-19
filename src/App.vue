d3792eed2e246e307440c015dafa664b30607b20
hamflx <a@hamflx.cn>
2022-07-19 22:59:40 +0800

    提高 diff 算法准确度

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,75 @@
 <script setup>
-import * as Y from 'yjs'
 import { newDocState, oldDocState } from '@/data'
 import { diffEditor } from '@hamflx/prosemirror-diff'
 import { EditorContent } from '@tiptap/vue-3'
-import { onMounted, ref } from 'vue'
+import {onMounted, ref, shallowRef, watchEffect} from 'vue'
 import {createEditor} from "./editor.js";
 
 const versionList = ref([])
 const ready = ref(false)
-
-const editor = ref(null)
+const currentTabName = ref('diff')
+const editor = shallowRef(null)
 
 onMounted(() => {
   editor.value = createEditor()
+  window.editor = editor.value
 
-  const docNode = diffEditor(editor.value.schema, oldDocState, newDocState)
+  editor.value.on('update', ({editor})=>{
+    localStorage.setItem('editor-data-' + currentTabName.value, JSON.stringify(editor.getJSON()))
+  })
+
+  reloadDiff()
+})
+
+watchEffect(() => {
+  const doc = versionList.value.find(v => v.name === currentTabName.value)?.doc
+  if (doc && ready.value) {
+    setTimeout(() => {
+      editor.value.commands.setContent(JSON.parse(JSON.stringify(doc)))
+    })
+  }
+})
+
+const reloadDiff = () => {
+  const oldSavedState = getSavedJson('editor-data-old') || oldDocState
+  const newSavedState = getSavedJson('editor-data-new') || newDocState
+
+  const docNode = diffEditor(editor.value.schema, oldSavedState, newSavedState)
+  window.oldDoc = oldSavedState
+  window.newDoc = newSavedState
+  console.log('==> oldSavedState', oldSavedState)
+  console.log('==> newSavedState', newSavedState)
   console.log('==> docNode', docNode)
-
-  editor.value.commands.setContent(docNode.toJSON())
 
   ready.value = true
   versionList.value = [
-    { name: 'old', doc: oldDocState },
-    { name: 'new', doc: newDocState },
+    { name: 'old', doc: oldSavedState },
+    { name: 'new', doc: newSavedState },
     { name: 'diff', doc: docNode.toJSON() }
   ]
-})
+}
 
-const handleSwitchVersion = () => {}
+const handleSwitchVersion = ver => {
+  if (ver.name === 'diff') {
+    reloadDiff()
+  }
+  currentTabName.value = ver.name
+}
+
+const getSavedJson = key => {
+  const value = localStorage.getItem(key)
+  if (value) {
+    return JSON.parse(value)
+  }
+  return undefined
+}
 </script>
 
 <template>
   <div>
     <div>
       <template v-for="ver of versionList" :key="ver.name">
-        <button @click="handleSwitchVersion(ver)">{{ ver.name }}</button>
+        <button :class="{ isActive: currentTabName === ver.name }" @click="handleSwitchVersion(ver)">{{ ver.name }}</button>
       </template>
     </div>
     <EditorContent v-if="ready" class="editor__content" :editor="editor" />
@@ -42,5 +77,8 @@ const handleSwitchVersion = () => {}
 </template>
 
 <style scoped>
-
+.isActive {
+  background-color: rgb(234, 0, 94);
+  color: #fff;
+}
 </style>
